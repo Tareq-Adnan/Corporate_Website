a064c8aedd780ef0091457624704fdaa7a64625c
Tarekul Islam <68398767+Tareq-Adnan@users.noreply.github.com>
2021-04-18 21:50:35 +0600

    Form Update
    Form php code small update

--- php/form.php
+++ php/form.php
@@ -1,55 +1,34 @@
 <?php
+$hostname='localhost';
+$password='';
+$username='root';
+$db_name='corporate';
+
+$conn= new mysqli($hostname,$username,$password,$db_name);
+
+if($conn->connect_error){
+    die("Connection failed.".$conn->connect_error);
+}
+
+if(isset($_POST['fullname']) && isset($_POST['username']) && isset($_POST['email']) && isset($_POST['phone']) && isset($_POST['gender'])){
 $fullname=$_POST['fullname'];
  $username=$_POST['username'];
  $email=$_POST['email'];
  $phone=$_POST['phone'];
- $interest=$_POST['interest'];
- $comment=$_POST['comment'];
+ $address=$_POST['address'];
  $gender=$_POST['gender'];
 
+    
+    $sql = "INSERT INTO register_info(full_name,user_name,email,phone,interests,gender) VALUES('$fullname','$username','$email','$phone','$address','$gender')";
 
- if(!empty($fullname) || !empty($username) || !empty($email) || !empty($phone) && !empty($interest) || !empty($username)){
-  
- 
-    $host="localhost";
-    $uname="root";
-    $pass="";
-    $dbname="corporate"
-   
-    $con= new mysqli($host, $uname, $pass, $dbname);
-
-    if(mysqli_connect_error()){
-        die('Connect Error('.mysqli_connect_errno().')'.mysqli_connect_error());
+    if(mysqli_query($conn,$sql)){
+        echo 'Successfully Inserted';
     }
     else{
-        $SELECT="SELECT email From register_info where email = ? Limit 1";
-        $INSERT="INSERT INTO register_info(full_name,username,email,phone,interests,comments,gender) VALUE(?,?,?,?,?,?,?)";
-
-        $stmot=$con->prepare($SELECT);
-        $stmt->bind_param("s",$email);
-        $stmt->execute();
-        $stmt->bind_result($email);
-        $stmt->store_result();
-        $rnum = $stmt->num_rows;
-
-        if($rnum==0){
-            $stmt->close();
-            $stmt->bind_param("sssisss",$fullname,$username,$email,$phone,$interest,$comment,$gender);
-            $stmt->execute();
-            echo "Successfully Registered";
-        }
-        else{
-            echo "Already Registered";
-        }
-        $stmt->close();
-        $con->close();
+        echo 'Failed';
     }
+}
 
-}
-else{
- echo "Please Fill Up the Form.";
- die();
-}
 
 
 ?>
